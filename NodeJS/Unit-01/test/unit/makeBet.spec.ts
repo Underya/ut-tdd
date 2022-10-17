60ea3f54ecd068f565d3ad427003b78836d0daaf
Askar <askar_ra@mail.ru>
2022-10-17 16:02:17 +0600

    added Players tests

--- NodeJS/Unit-01/test/unit/makeBet.spec.ts
+++ NodeJS/Unit-01/test/unit/makeBet.spec.ts
@@ -1,6 +1,8 @@
 //@ts-test
 import { expect } from 'chai'
 import { Chip } from '../../src/Chip'
+import { Player } from '../../src/Player'
+import { RollDiceGame } from '../../src/RollDiceGame'
 
 describe('test', () => {
   let chip = new Chip(1)
@@ -28,4 +30,56 @@ describe('Chips test', () => {
   it('both should equals', () => {
     expect(chip1.equals(chip2)).to.equal(true)
   })
+})
+
+describe('Player test', () => {
+  it('can join the game', () => {
+    let player = new Player()
+    let game = new RollDiceGame()
+
+    player.join(game)
+
+    expect(player.isInGame()).to.equal(true)
+  })
+
+  // it('cannot join two times', () => {
+  //   let player = new Player()
+  //   let game = new RollDiceGame()
+
+  //   player.join(game)
+  //   player.join(game)
+
+  //   expect(player.isInGame()).to.equal(false)
+  // })
+
+  // it('can leave game', () => {
+  //   let player = new Player()
+  //   let game = new RollDiceGame()
+
+  //   player.leaveGame()
+
+  //   expect(player.isInGame()).to.equal(false)
+  // })
+
+  it('seventh player cannot be added to the game', () => {
+    let game = createGameWithSixPlayers()
+    let player = new Player()
+
+    // expect(() => {
+    //   player.join(game)
+    // }).to.throw('Game can not accept more than 6 players')
+    expect(player.join(game)).to.throw(
+      'Game can not accept more than 6 players'
+    )
+  })
+
+  function createGameWithSixPlayers() {
+    let game = new RollDiceGame()
+
+    for (let index = 0; index < 6; index++) {
+      let player = new Player()
+      player.join(game)
+    }
+    return game
+  }
 })
