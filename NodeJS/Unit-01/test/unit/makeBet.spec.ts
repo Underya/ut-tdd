4109cff84fd2d47b5d75d1f3227c9888a4ff5e13
Askar <askar_ra@mail.ru>
2022-10-17 14:14:54 +0600

    added Chips test

--- NodeJS/Unit-01/test/unit/makeBet.spec.ts
+++ NodeJS/Unit-01/test/unit/makeBet.spec.ts
@@ -11,12 +11,21 @@ describe('test', () => {
   })
 })
 
-describe('add chip', () => {
-  let chip = new Chip(1)
+describe('Chips test', () => {
+  let resualtChip = new Chip(2)
   let chip1 = new Chip(1)
   let chip2 = new Chip(1)
   // let chip2 = new Chip(1)
-  it('should add a chip', () => {
-    expect(chip1.add(chip2)).to.equal(1)
+  it('should add one chip', () => {
+    expect(resualtChip.equals(chip1.add(chip2))).to.equal(true)
+  })
+  it('should multiply by 2', () => {
+    expect(resualtChip.equals(chip1.multiply(2))).to.equal(true)
+  })
+  it('first should grater than second', () => {
+    expect(resualtChip.gt(chip2)).to.equal(true)
+  })
+  it('both should equals', () => {
+    expect(chip1.equals(chip2)).to.equal(true)
   })
 })
